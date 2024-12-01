61eea295f0f5300aed4f0a1a3ce7c7716270ea81
martinnCasti <mj.castineira@gmail.com>
2024-12-01 01:28:15 -0300

    se borra carpeta payment

--- app/suscribe/payment/pending.tsx
+++
@@ -1,8 +0,0 @@
-export default function Pending() {
-  return (
-    <div>
-      <h1>Tu pago esta pendiente de aprobacion</h1>
-      // Aquí puedes mostrar los detalles de la transacción
-    </div>
-  );
-}
--- app/suscribe/payment/success.tsx
+++
@@ -1,8 +0,0 @@
-export default function Success() {
-  return (
-    <div>
-      <h1>¡Pago exitoso!</h1>
-      // Aquí puedes mostrar los detalles de la transacción
-    </div>
-  );
-}

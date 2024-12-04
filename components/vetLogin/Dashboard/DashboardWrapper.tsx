8599e0071da646ed8d3ab0c07de3c990f77d5075
martinnCasti <mj.castineira@gmail.com>
2024-12-04 02:46:24 -0300

    fix integracion con MP-paymentId

--- components/vetLogin/Dashboard/DashboardWrapper.tsx
+++ components/vetLogin/Dashboard/DashboardWrapper.tsx
@@ -7,13 +7,6 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { processPaymentStatus } from "@/src/services/userServices";
 
-interface PreapprovalData {
-  type: string;
-  id: string;
-  status: string;
-  paymentId: string;
-}
-
 export default function Page() {
   const {
     isStatusDisabled: isDisabled,
@@ -26,28 +19,21 @@ export default function Page() {
 
   useEffect(() => {
     const processMercadoPagoPayment = async () => {
-      console.log("Procesando pago de Mercado Pago...");
+      const paymentId = searchParams.get("payment_id");
+      const status = searchParams.get("status");
 
-      const preapproval = searchParams.get("preapproval");
-      if (!preapproval) {
-        console.log("No hay datos de preapproval");
+      console.log("Payment ID:", paymentId);
+      console.log("Status:", status);
+
+      if (!paymentId || status !== "approved") {
+        console.log("No hay payment_id o el status no es approved");
         return;
       }
 
+      setIsProcessingPayment(true);
+      setPaymentError(null);
+
       try {
-        const preapprovalData: PreapprovalData = JSON.parse(
-          decodeURIComponent(preapproval)
-        );
-        console.log("Datos de preapproval:", preapprovalData);
-
-        if (!preapprovalData.paymentId) {
-          console.log("No se encontró paymentId en los datos");
-          return;
-        }
-
-        setIsProcessingPayment(true);
-        setPaymentError(null);
-
         const vetId = localStorage.getItem("vetId");
         if (!vetId) {
           throw new Error("No se encontró el ID del veterinario");
@@ -55,10 +41,11 @@ export default function Page() {
 
         await processPaymentStatus({
           vetId: parseInt(vetId),
-          paymentId: preapprovalData.paymentId,
+          paymentId,
         });
 
         await checkStatus();
+        console.log("Pago procesado exitosamente");
       } catch (error) {
         console.error("Error:", error);
         setPaymentError(
